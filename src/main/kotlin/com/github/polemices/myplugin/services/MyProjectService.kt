5fd4cf90a277fc37af1a55ac9761d8cbffea063c
GitHub Action <action@github.com>
2022-07-12 10:04:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/polemices/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/polemices/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.polemices.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.polemices.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
